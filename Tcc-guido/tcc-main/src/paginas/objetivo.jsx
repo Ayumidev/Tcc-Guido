63dfd55707e8b2b2007b70ad2d5efdc61bd9eef5
Ayumi <79387837+Ayumidev@users.noreply.github.com>
2023-10-19 00:15:02 -0300

    .

--- Tcc-guido/tcc-main/src/paginas/objetivo.jsx
+++ Tcc-guido/tcc-main/src/paginas/objetivo.jsx
@@ -1,34 +1,41 @@
-import { Link } from 'react-router-dom'
 import './css/Objetivo.css'
+
+import PB from '../imagens/olhoalvo.png'
+import OB from '../imagens/objetivo.png'
 
 function Objetivo() {
     return (
     <>
 
-<header><h1>Objetivo</h1></header>
- 
-        
+    <header>
+        <div className="obj">
+            <div className='img'>
+                <img src={OB} />
+                <br />
+                <h1>Objetivo</h1>
+                <br />
+            </div>
+            
+            <p>
+                O nosso pensamento na criação do guido foi facilitar e deixar mas seguro o transporte desses documentos, que
+                podem ser perdidos e até danificados,oque pode ser prejudicial.
+            </p>
+         </div>
 
-<div className="obj">
-    <p>
-    O nosso pensamento na criação do guido foi facilitar e deixar mas seguro o transporte desses documentos, que
-    podem ser perdidos e até danificados,oque pode ser prejudicial.
-    </p>
-
-    </div>
-
-
-
-    <header><h1>Público Alvo</h1></header>
-    
-
-<div className="conteudop">
-   <p>
-    O tipo de cliente que buscamos são escritórios que buscam econimizar tempo transportando documentos.
-    Mas é nosso produto pode se adaptar aos interessados de outras áreas.
-   </p>
+         <div className="conteudop">
+         <h1>Público Alvo</h1>
+         <div className='img'>
+                <img src={PB} />
+            </div>
+                <p>
+                 O tipo de cliente que buscamos são escritórios que buscam econimizar tempo transportando documentos.
+                 Mas é nosso produto pode se adaptar aos interessados de outras áreas.
+                </p>
    
    </div>
+    </header>
+ 
+        
 
 
 </>
